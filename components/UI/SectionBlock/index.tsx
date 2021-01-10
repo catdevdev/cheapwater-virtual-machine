98cad65ccbeb6e68022bc2f7d3b55812e4052721
catdevdev <catdev.dev@gmail.com>
2021-01-10 23:15:02 +0200

    updated "SectionBlock" component

--- components/UI/SectionBlock/index.tsx
+++ components/UI/SectionBlock/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default styled.div<Props>`
   ${({ style }) => style ?? {}};
-  width: 300px;
+  width: 100%;
   display: inline-block;
   border: 1px solid #c0c0c0;
   border-radius: 5px;
