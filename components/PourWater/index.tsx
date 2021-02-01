76c6b5eea3822fe0f829404938896189c740378f
catdevdev <catdev.dev@gmail.com>
2021-02-01 23:31:47 +0200

    finish local PourWater

--- components/PourWater/index.tsx
+++ components/PourWater/index.tsx
@@ -1,23 +1,54 @@
 /* imports */
-import styled from "styled-components";
 import c from "./index.module.scss";
+import { useState } from "react";
 /* UI */
 /* 0 */
-import { Row, Col, Label, Input } from "reactstrap";
+import { Label, Input } from "reactstrap";
 /* 1 */
 import Container from "../../components/UI/Container";
 import SectionBlock from "../../components/UI/SectionBlock";
 import Button from "../../components/UI/Button";
+/* redux */
+import { useDispatch } from "react-redux";
+import {
+  addWaterInMachine,
+  setWaterInMachine,
+} from "../../redux/actions/waterStatus";
+/* redux types */
+import { StoreState } from "../../redux/reducers";
 
 const PourWater = () => {
+  const dispatch = useDispatch();
+  const [volumeLocalWater, setLocalWater] = useState("0");
+
   return (
     <div className={c.wrapper}>
       <SectionBlock>
         <div className={c.pourWaterBlock}>
           <Label>Pour water into a machine (local)</Label>
-          <Button style={{ marginTop: 14 }}>Add</Button>
-          <Button style={{ marginTop: 14 }}>Set</Button>
-          <Input placeholder="Enter water volume" style={{ marginTop: 14 }} />
+          <Button
+            onClick={() => {
+              dispatch(addWaterInMachine(+volumeLocalWater));
+            }}
+            style={{ marginTop: 14 }}
+          >
+            Add
+          </Button>
+          <Button
+            onClick={() => {
+              dispatch(setWaterInMachine(+volumeLocalWater));
+            }}
+            style={{ marginTop: 14 }}
+          >
+            Set
+          </Button>
+          <Input
+            onChange={(e) => {
+              setLocalWater(e.target.value);
+            }}
+            placeholder="Enter water volume"
+            style={{ marginTop: 14 }}
+          />
         </div>
       </SectionBlock>
       <SectionBlock style={{ marginLeft: 14 }}>
