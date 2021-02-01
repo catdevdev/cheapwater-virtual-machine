0542f2ab4b0323f76e6df47b129c1ec639f8e978
catdevdev <catdev.dev@gmail.com>
2021-02-01 21:24:30 +0200

    finish getWaterInMachine action

--- redux/actions/waterStatus/index.ts
+++ redux/actions/waterStatus/index.ts
@@ -1,38 +1,44 @@
 /* imports */
 import { Dispatch } from "redux";
 /* api */
-import { patchStatusMachine, getStatusMachine } from "../../api/statusMachine";
+import { getWaterStatus } from "../../api/waterStatus";
 /* types */
-import { ActionTypes } from "./types";
+import {
+  GetWaterInMachine,
+  AddWaterInMachine,
+  SetWaterInMachine,
+} from "./types";
 import { WaterStatus } from "../../api/waterStatus";
 
 export interface GetWaterInMachineAction {
-  type: ActionTypes;
-}
-export interface SetWaterInMachineAction {
-  type: ActionTypes.SetWaterInMachine;
+  type: typeof GetWaterInMachine;
   payload: WaterStatus;
 }
-export interface AddWaterInMachineAction {
-  type: ActionTypes.AddWaterInMachine;
-  payload: WaterStatus;
-}
+// export interface SetWaterInMachineAction {
+//   type: ActionTypes.SetWaterInMachine;
+//   payload: WaterStatus;
+// }
+// export interface AddWaterInMachineAction {
+//   type: ActionTypes.AddWaterInMachine;
+//   payload: WaterStatus;
+// }
 
 export const getWaterInMachine = () => {
   return async (dispatch: Dispatch) => {
-    const response = await getStatusMachine();
+    const response = await getWaterStatus();
     dispatch<GetWaterInMachineAction>({
-      type: ActionTypes.GetWaterInMachine,
-    });
-  };
-};
-
-export const setWaterInMachine = () => {
-  return async (dispatch: Dispatch) => {
-    const response = await getStatusMachine();
-    dispatch<SetWaterInMachineAction>({
-      type: ActionTypes.SetWaterInMachine,
+      type: GetWaterInMachine,
       payload: response.data,
     });
   };
 };
+
+// export const setWaterInMachine = () => {
+//   return async (dispatch: Dispatch) => {
+//     const response = await getStatusMachine();
+//     dispatch<SetWaterInMachineAction>({
+//       type: ActionTypes.SetWaterInMachine,
+//       payload: response.data,
+//     });
+//   };
+// };
--- redux/actions/waterStatus/types.ts
+++ redux/actions/waterStatus/types.ts
@@ -1,20 +1,23 @@
-export const ActionTypes: {
-  AddWaterInMachine;
-  SetWaterInMachine;
-  GetWaterInMachine;
-} = {
-  AddWaterInMachine: "AddWaterInMachine",
-  SetWaterInMachine: "SetWaterInMachine",
-  GetWaterInMachine: "GetWaterInMachine",
-};
+// export const ActionTypes: {
+//   AddWaterInMachine: string;
+//   SetWaterInMachine: string;
+//   GetWaterInMachine;
+// } = {
+//   AddWaterInMachine: "ADD_WATER_IN_MACHINE",
+//   SetWaterInMachine: "SET_WATER_IN_MACHINE",
+//   GetWaterInMachine: "GET_WATER_IN_MACHINE",
+// };
+
+export const AddWaterInMachine = "ADD_WATER_IN_MACHINE";
+export const SetWaterInMachine = "SET_WATER_IN_MACHINE";
+export const GetWaterInMachine = "GET_WATER_IN_MACHINE";
 
 import {
   GetWaterInMachineAction,
-  SetWaterInMachineAction,
-  AddWaterInMachineAction,
+  // SetWaterInMachineAction,
+  // AddWaterInMachineAction,
 } from "./index";
 
-export type Action =
-  | GetWaterInMachineAction
-  | SetWaterInMachineAction
-  | AddWaterInMachineAction;
+export type Action = GetWaterInMachineAction;
+// | SetWaterInMachineAction
+// | AddWaterInMachineAction;
