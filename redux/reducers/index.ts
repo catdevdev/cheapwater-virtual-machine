03185899f9cebef3b35c38ddd189cac039c096ba
catdevdev <catdev.dev@gmail.com>
2021-02-01 22:15:53 +0200

    added "waterStatus" reducer to store

--- redux/reducers/index.ts
+++ redux/reducers/index.ts
@@ -1,11 +1,17 @@
+/* types */
 import { StatusMachine } from "./../api/statusMachine";
+import { WaterStatus } from "./../api/waterStatus";
+/* redux stuffs */
 import { combineReducers } from "redux";
 import { statusMachine } from "./statusMachine";
+import { waterStatus } from "./waterStatus";
 
 export interface StoreState {
   statusMachine: StatusMachine;
+  waterStatus: WaterStatus;
 }
 
 export const rootReducer = combineReducers<StoreState>({
   statusMachine,
+  waterStatus,
 });
